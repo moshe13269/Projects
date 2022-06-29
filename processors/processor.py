04e9f3fc4d0e2fe80282b2ad853835ee12aaafea
MosheL <Moshe.Laufer@rada.com>
2022-06-30 00:06:20 +0300

    Initial commit

--- processors/processor.py
+++ processors/processor.py
@@ -1,11 +1,14 @@
-from tensorflow import keras
 import numpy as np
+import tensorflow as tf
+from tensorflow import keras
 
 
 class Processor(keras.utils.Sequence):
 
-    def __init__(self, x_in, batch_size, y_in=None, shuffle=True):
+    def __init__(self, x_in, batch_size, num2mask, t_axis, y_in=None, shuffle=True):
         # Initialization
+        self.num2mask = num2mask
+        self.t_axis = t_axis
         self.batch_size = batch_size
         self.shuffle = shuffle
         self.x = x_in
@@ -15,11 +18,17 @@ class Processor(keras.utils.Sequence):
         if self.shuffle:
             np.random.shuffle(self.indexes)
 
+    def craete_mask(self, batch_shape):
+        random = tf.random.normal(shape=(batch_shape, self.t_axis))
+        point2mask, _ = tf.math.top_k(random, self.num2mask)
+        mask = tf.where(random - tf.reduce_min(point2mask, axis=-1, keepdims=True) >= 0., 1., 0.)
+        return mask
+
     def __getitem__(self, index):
         # get batch indexes from shuffled indexes
         batch_indexes = self.indexes[index * self.batch_size:(index + 1) * self.batch_size]
         x_batch = self.x[batch_indexes]
-        y_batch = self.y[batch_indexes]
+        y_batch = self.craete_mask(x_batch[0].shape[0])
         return x_batch, y_batch
 
     def __len__(self):
