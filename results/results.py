539fc29a3a5abf71e7482bb4e5a44dc47189e97a
moshe <moshe13269@gmail.com>
2022-09-23 00:10:28 +0300

    init commit

--- results/results.py
+++ results/results.py
@@ -1,6 +1,25 @@
 
 class Results:
 
-    def __init__(self):
+    path2save_results: str
+    num_class: int
+
+    def __init__(self, path2save_results: str,
+                 num_class: int):
         self.path2save_results = path2save_results
+        self.num_class = num_class
+
+    def plot_ROC_OvR(self):
+        pass
+
+    def plot_ROC_OvO(self):
+        pass
+
+    def confusion_matrix(self):
+        pass
+
+
+
+
+
 
