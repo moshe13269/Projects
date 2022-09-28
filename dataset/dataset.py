e11c6b2f78cd512c7433c144212befdfd8920cf3
moshe <moshe13269@gmail.com>
2022-09-29 00:02:08 +0300

    init commit

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -1,7 +1,9 @@
+
 import os
+import sys
 from os import listdir
 from os.path import isfile, join
-import numpy as np
+from dataclasses import dataclass
 
 """
 1. dataset folder contained a two folders: dataset and labels
@@ -10,24 +12,42 @@ import numpy as np
 """
 
 
+@dataclass
 class Dataset:
     path2load: str
     type_files: str
+    labels: bool
 
-    def __init__(self, path2load, type_files):
+    """class for load path to samples of dataset and the labels (if had a labels)"""
+
+    def __init__(self, path2load, type_files, labels=False):
         assert os.path.isdir(path2load), 'Directory not exist'
 
         self.path2load = path2load
-        self.dataset_names = [file for file in listdir(join(path2load, 'dataset'))
+        self.dataset_names = [join(path2load, 'dataset', file) for file in listdir(join(path2load, 'dataset'))
                               if isfile(join(path2load, 'dataset', file)) and
                               os.path.splitext(file)[-1].lower() == type_files]
+        if labels:
+            self.labels_names = None
+        else:
+            self.labels_names = [join(path2load, 'labels', file) for file in listdir(join(path2load, 'labels'))
+                                 if isfile(join(path2load, 'labels', file)) and
+                                 os.path.splitext(file)[-1].lower() == type_files]
 
-        self.labels_names = [file for file in listdir(join(path2load, 'labels'))
-                             if isfile(join(path2load, 'labels', file)) and
-                             os.path.splitext(file)[-1].lower() == type_files]
+        if labels:
+            def check_dataset():
 
-        def check_dataset():
-            assert len(self.dataset_names) == len(self.labels_names), print('The number of dataset and labels ineqal')
-            for index in range(len(self.dataset_names)):
-                if self.dataset_names[index].replace('dataset', '') == self.labels_names[index].replace('labels', ''):
+                dataset_type_file = os.path.splitext(self.dataset_names[0])[-1].lower()
+                label_type_file = os.path.splitext(self.labels_names[0])[-1].lower()
 
+                assert len(self.dataset_names) == len(self.labels_names), \
+                    print('The number of data samples and the labels is unequal')
+                for index in range(len(self.dataset_names)):
+                    if self.dataset_names[index].replace('dataset', '').replace(dataset_type_file, '') \
+                            != self.labels_names[index].replace('labels', '').replace(label_type_file, ''):
+                        sys.exit('The names of the files in the index: %d is not the same name in the dataset and the '
+                                 'labels lists\n Dataset: %s\n Labels: %s'
+                                 % (index, self.dataset_names[index], self.labels_names[index]))
+
+            check_dataset()
+            # self.dataset_names = list(zip(self.dataset_names, self.labels_names))
--- task/train_test_task.py
+++ task/train_test_task.py
@@ -1,10 +1,11 @@
-
 import mlflow
 import mlflow.keras
 import tensorflow as tf
 from hydra.utils import instantiate
-from processors.processor import Processor
+# from tensorflow.data import AUTOTUNE
 from omegaconf import DictConfig, OmegaConf
+from sklearn.model_selection import train_test_split
+
 # from model.self_supervised_model import Wav2Vec
 # from losses.diversity_loss import DiversityLoss
 # from losses.contrastive_loss import ContrastiveLoss
@@ -16,30 +17,106 @@ class TrainTask:
         self.cfg = cfg
         self.model = instantiate(cfg.model)
 
-        self.path2save_model = self.cfg.get('path2save_model')
-        self.path2load_dataset = self.cfg.get('path2load_dataset')
+        self.datasets_path = instantiate(cfg.dataset_class)
 
-        self.processor_train = Processor()
-        self.processor_validation = Processor()
+        self.train_dataset = None
+        self.test_dataset = None
+        self.val_dataset = None
+
+        self.path2save_model = self.cfg.get('path2save_model')
+
+        self.model = instantiate(cfg.model)
         self.loss = instantiate(cfg.losses)
         self.epochs = self.cfg.get('epochs')
+        self.callbacks = instantiate(cfg.callbacks)
+        self.optimizer = instantiate(cfg.optimizer)
+        self.train_steps_per_epoch = self.cfg.get('train_steps_per_epoch')
+
+        self.processor = instantiate(cfg.processor)
+        self.batch_size = self.cfg.get('batch_size')
+
+        self.results = instantiate(cfg.results)
 
     def run(self):
+
+        if self.datasets_path.labels_names is None:
+            X_train, X_test, _, y_test = train_test_split(self.datasets_path.dataset_names,
+                                                          self.datasets_path.dataset_names,
+                                                          test_size=0.2,
+                                                          random_state=1)
+
+            X_test, X_val, y_test, y_val = train_test_split(X_test,
+                                                            y_test,
+                                                            test_size=0.05,
+                                                            random_state=1)
+
+            self.train_dataset = tf.data.Dataset.from_tensor_slices(X_train)
+            self.test_dataset = tf.data.Dataset.from_tensor_slices(X_test)
+            self.val_dataset = tf.data.Dataset.from_tensor_slices(X_val)
+
+        else:
+            X_train, X_test, y_train, y_test = train_test_split(self.datasets_path.dataset_names,
+                                                                self.datasets_path.labels_names,
+                                                                test_size=0.2,
+                                                                random_state=1)
+
+            X_test, X_val, y_test, y_val = train_test_split(X_test,
+                                                            y_test,
+                                                            test_size=0.05,
+                                                            random_state=1)
+
+            self.train_dataset = tf.data.Dataset.from_tensor_slices(list(zip(X_train, y_train)))
+            self.test_dataset = tf.data.Dataset.from_tensor_slices(list(zip(X_test, y_test)))
+            self.val_dataset = tf.data.Dataset.from_tensor_slices(list(zip(X_val, y_val)))
+
+        self.train_dataset = (self.train_dataset
+                              .shuffle(1024)
+                              .map(self.processor, num_parallel_calls=tf.data.AUTOTUNE)
+                              .cache()
+                              .repeat()
+                              .batch(self.batch_size['train'])
+                              .prefetch(tf.data.AUTOTUNE)
+                              )
+
+        self.test_dataset = (self.test_dataset
+                             .shuffle(1024)
+                             .map(self.processor, num_parallel_calls=tf.data.AUTOTUNE)
+                             .cache()
+                             .repeat()
+                             .batch(self.batch_size['test'])
+                             .prefetch(tf.data.AUTOTUNE)
+                             )
+
+        self.val_dataset = (self.val_dataset
+                            .shuffle(1024)
+                            .map(self.processor, num_parallel_calls=tf.data.AUTOTUNE)
+                            .cache()
+                            .repeat()
+                            .batch(self.batch_size['valid'])
+                            .prefetch(tf.data.AUTOTUNE)
+                            )
+
         self.model.compile(optimizer="Adam", loss=self.loss)
 
         mlflow.keras.autolog()
 
-        self.model.fit(x=self.processor_train, epochs=self.epochs, verbose=1, validation_data=self.processor_validation,
-                       initial_epoch=0, use_multiprocessing=True)
+        self.model.fit(x=self.train_dataset,
+                       epochs=self.epochs,
+                       verbose=1,
+                       validation_data=self.val_dataset,
+                       callbacks=self.callbacks,
+                       steps_per_epoch=self.train_steps_per_epoch,
+                       initial_epoch=0,
+                       use_multiprocessing=True)
 
-        tf.saved_model.save(self.model, self.path2save_model)
+        tf.saved_model.save(self.model,
+                            self.path2save_model)
 
 
 if __name__ == '__main__':
     task = TrainTask(epochs=3, path2save_model=r"C:\Users\moshel\Desktop\cscscs",
                      path2load_dataset=r"C:\Users\moshel\Desktop\cscscs")
     task.run()
-
 
 # class TrainValTestTask(BaseTask):
 #
