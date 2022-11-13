f5d9fa9298a3af83473583aca52288810cd1953c
moshelaufer <moshe13269@gmail.com>
2022-11-13 13:32:09 +0200

    sfdsafdsfs

--- main/main_data2vec_wav.py
+++ main/main_data2vec_wav.py
@@ -1,19 +1,17 @@
+# import sys
+#
+# sys.path.append('../task/')
 import os
 import task
 import hydra
 from omegaconf import DictConfig, OmegaConf
-from task import TrainTask
-
 
 
 @hydra.main(config_path=os.path.join('../config', 'data2vec_wav'), config_name='config')
 def main(cfg: DictConfig) -> None:
-    train_task = task.TrainTask(cfg)
+    train_task = task.TrainTestTask(cfg)
     train_task.run()
 
 
 if __name__ == "__main__":
-    # from tensorflow.python.client import device_lib
-    #
-    # print(device_lib.list_local_devices())
     main()
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,2 @@
 
-from task.train_test_task import TrainTask
-from task.fine_tuning_task import TrainTask
+from utils.utils import outputs_conv_size
