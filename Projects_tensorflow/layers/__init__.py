1b65e742248d7af3b24022a9a2e3862f8e0b37d3
moshe <moshe13269@gmail.com>
2023-04-13 00:21:56 +0300

    init commit

--- Projects_tensorflow/layers/__init__.py
+++ Projects_tensorflow/layers/__init__.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.append('/home/moshela/work/moshe/pycharm/Projects/')
 from Projects_tensorflow.layers.transformer import Transformer as Transformer
 from Projects_tensorflow.layers.conv_wav_encoder import ConvFeatureExtractionModel
 from Projects_tensorflow.layers.conv_wav_decoder import ConvDecoderModel
--- Projects_tensorflow/model/__init__.py
+++ Projects_tensorflow/model/__init__.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.append('/home/moshela/work/moshe/pycharm/Projects/')
 from Projects_tensorflow.model.synth_autoencoder_model_2_stft import SynthAutoEncoder
 from Projects_tensorflow.model.synth_autoencoder_model_2_stft_v2 import SynthAutoEncoder
 # from Projects_tensorflow.model.ssl_synth_model import SSLSynthModel
