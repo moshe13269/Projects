05053ec694247d66a283016782a99e25b647d08b
moshe <moshe13269@gmail.com>
2023-05-21 23:02:12 +0300

    init commit

--- Projects_torch/task/train_test_supervised.py
+++ Projects_torch/task/train_test_supervised.py
@@ -91,7 +91,7 @@ class TrainTestTaskSupervised:
 
                 num_steps = 0
 
-                for step, (inputs, labels) in enumerate(self.dataloader, 1):
+                for step, (inputs, labels) in enumerate(self.dataloader): #, 1
                     if torch.cuda.is_available():
                         labels = labels.cuda()
                     if isinstance(inputs, list) or isinstance(inputs, tuple):
@@ -128,7 +128,7 @@ class TrainTestTaskSupervised:
 
                     num_steps += 1
 
-                    if num_steps > 0 and num_steps % 10000:
+                    if num_steps > 0 and num_steps % 30000:
                         print('loss_param: %f, loss_stft: %f'
                               % (running_loss_parmas_counter/num_steps, running_loss_stft_counter/num_steps))
 
