cc81d24b21a7314631fb8608220d82003484e5de
moshe <moshe13269@gmail.com>
2023-07-31 23:45:42 +0300

    init commit

--- Projects_torch/layers/__init__.py
+++ Projects_torch/layers/__init__.py
@@ -3,6 +3,6 @@ from Projects_torch.layers.conv_wav_encoder import ConvFeatureExtractionModel
 from Projects_torch.layers.conv_wav_decoder import ConvDecoderModel
 from Projects_torch.layers.linear_classifier_2 import LinearClassifier
 from Projects_torch.layers.unet_blocks import ConvBlock, UpConvBlock, AttentionBlock
-from Projects_torch.layers.transformer import Transformer
+# from Projects_torch.layers.transformer import Transformer
 from Projects_torch.layers.transformer import TransformerE
 from Projects_torch.layers.transformer_decoder import TransformerD
