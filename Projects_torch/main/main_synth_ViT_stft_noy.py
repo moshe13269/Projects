2f046ba06ba8198a59d2698e532a548a24f4ddb4
moshe <moshe13269@gmail.com>
2023-09-24 00:21:03 +0300

    init commit

--- Projects_torch/main/main_synth_ViT_stft_noy.py
+++ Projects_torch/main/main_synth_ViT_stft_noy.py
@@ -23,10 +23,12 @@ path2save: path to empty folder which all the outputs will be saved
 def main(cfg: DictConfig) -> None:
     parser = argparse.ArgumentParser(description=None)
     parser.add_argument('--path2data', required=False,
-                        default=r'C:\Users\moshe\PycharmProjects\commercial_synth_dataset\noy',
+                        default='/home/a/home/cc/students/cs/moshelaufer/dataset/fm',
+                        # default=r'C:\Users\moshe\PycharmProjects\commercial_synth_dataset\noy',
                         help='path2data')
     parser.add_argument('--path2save', required=False,
-                        default=r'C:\Users\moshe\PycharmProjects\commercial_synth_dataset\outputs_main',
+                        default='/home/a/home/cc/students/cs/moshelaufer/outputs',
+                        # default=r'C:\Users\moshe\PycharmProjects\commercial_synth_dataset\outputs_main',
                         help='path2save')
 
     args = parser.parse_args()
