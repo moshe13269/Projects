6316460c695aade9bcb6f9b2d8a0fa7da1395b71
moshe <moshe13269@gmail.com>
2023-09-24 04:28:50 +0300

    init commit

--- Projects_torch/processors/processor_synth_encoder_eml_spec.py
+++ Projects_torch/processors/processor_synth_encoder_eml_spec.py
@@ -40,14 +40,17 @@ class DataLoaderMelSpec(Dataset):
                  calc_mean=False):
 
         super().__init__()
+        print('1111111111111111111')
         self.norm_mean = norm_mean
         self.norm_std = norm_std
         self.dataset_path = None
         self.num_classes = num_classes
+        print('2222222222222222222222222222222')
         self.num_classes_per_outputs = np.asarray(
             [sum(num_classes[:i])
              for i in range(len(num_classes))]
         )
+        print('33333333333333333333333333')
         self.files = None
         self.files_ = None
         self.labels = None
@@ -56,6 +59,7 @@ class DataLoaderMelSpec(Dataset):
         self.encoder = encoder
         self.win_length=win_length
         self.n_fft=n_fft
+        print('44444444444444444444444444444444444')
 
     def load_dataset(self, dataset_path):
         self.files = [os.path.join(dataset_path, file)
